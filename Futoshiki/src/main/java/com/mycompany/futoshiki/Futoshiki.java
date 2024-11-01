eae3f4a4c021cdd101d9eac5efdedbc34b052d3c
ximemolina <ximenamolinaportilla01@gmail.com>
2024-11-01 10:15:54 -0600

    creacion proyecto

---
+++ Futoshiki/src/main/java/com/mycompany/futoshiki/Futoshiki.java
@@ -0,0 +1,12 @@
+package com.mycompany.futoshiki;
+
+/**
+ *
+ * @author ximena molina - juan pablo cambronero
+ */
+public class Futoshiki {
+
+    public static void main(String[] args) {
+
+    }
+}
